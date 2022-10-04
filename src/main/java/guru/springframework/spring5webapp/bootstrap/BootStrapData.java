41b23db2b23945d19b53def78ad77efc6e45b4e0
Sijie Shen <shen.sij@northeastern.edu>
2022-10-04 12:31:16 -0400

    Add Publisher to JPA entity

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.domain.Author;
 import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -11,29 +13,48 @@ import org.springframework.stereotype.Component;
 public class BootStrapData implements CommandLineRunner {
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        Publisher publisher = new Publisher();
+        publisher.setCity("Santa Clara");
+        publisher.setAddressLine1("Amazon California Office");
+        publisher.setName("Amazon");
+        publisher.setZip("02148");
+        publisher.setState("California");
+        publisherRepository.save(publisher);
+
         Author eric = new Author("Eric", "Evans");
         Book ddd = new Book("Domain Driven Design", "123123");
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
+        ddd.setPublisher(publisher);
+        publisher.getBooks().add(ddd);
         authorRepository.save(eric);
         bookRepository.save(ddd);
+        publisherRepository.save(publisher);
+
 
         Author Jeffery = new Author("Jeffery", "Bezos");
         Book Offer = new Book("Offer for Sijie Shen in Amazon", "010101");
         Jeffery.getBooks().add(Offer);
         Offer.getAuthors().add(Jeffery);
+        Offer.setPublisher(publisher);
+        Offer.setPublisher(publisher);
+        publisher.getBooks().add(Offer);
         authorRepository.save(Jeffery);
         bookRepository.save(Offer);
+        publisherRepository.save(publisher);
 
         System.out.println("Started in Bootstrap");
         System.out.println("Number of Books: " + bookRepository.count());
+        System.out.println("Publisher Number of Books: " + publisher.getBooks().size());
     }
 }

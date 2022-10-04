ae2de22a62d426663e032db492640cd46e7d748b
Sijie Shen <shen.sij@northeastern.edu>
2022-10-04 13:05:24 -0400

    before controllers

---
+++ src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
@@ -0,0 +1,11 @@
+package guru.springframework.spring5webapp.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class BookController {
+    public String getBooks() {
+
+        return "books";
+    }
+}
